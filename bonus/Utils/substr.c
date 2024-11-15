4c9ac1ca0fa3509322545646b7ba4b875fcc6531
ajabri <ajabri@e1r7p1.1337.ma>
2024-11-15 11:30:19 +0100

    The bonus is starting

---
+++ bonus/Utils/substr.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   substr.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 10:15:47 by ajabri            #+#    #+#             */
+/*   Updated: 2024/10/12 12:37:11 by kali             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Header/cub3d.h"
+
+static char	*ft_while(char *ptr, char *st, unsigned int star, size_t lent)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (st[i] && j < lent)
+	{
+		if (i >= star)
+		{
+			ptr[j] = st[i];
+			j++;
+		}
+		i++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	if (start >= (unsigned int)ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	ft_while(str, s, start, len);
+	return (str);
+}
